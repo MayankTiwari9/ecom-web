66b924cdc9f64531afdf939386bdc5bb11abb3d4
Mayank tiwari <mt1027319@gmail.com>
2023-04-14 19:48:45 +0530

    Product card Home

--- frontend/src/Components/Home/ProductCard.js
+++ frontend/src/Components/Home/ProductCard.js
@@ -1,26 +1,37 @@
 import React from 'react'
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import "./Home.css";
 
 const ProductCard = () => {
   const products = useSelector((state) => state.allProducts.products);
   console.log(products);
 
   const renderList = products.map((product) => {
-    const {id,name,price} = product;
+    const { id, name, price, image, category, description } = product;
 
-    return(
-        <div>
-            <div>{id}</div>
-            <div>{name}</div>
-            <div>{price}</div>
+    return (
+      <div>
+        <div className='productCard-container'>
+          <div className='productCard-image'>
+            <img src={image} alt={id} />
+          </div>
+          <div className='productCard-details'>
+            <h2>{name}</h2>
+            <h4>{price}</h4>
+            <p>{category}</p>
+            <p>{description}</p>
+          </div>
         </div>
+      </div>
     )
   })
 
   return (
     <>
-    <h1>ProductCard</h1>
-    {renderList}
+      <h1>ProductCard</h1>
+      <div className='productcart-main'>
+        {renderList}
+      </div>
     </>
 
   )
