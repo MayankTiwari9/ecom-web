1b0c1c77bfa66f429dbb788297628f25f1c163b9
Mayank tiwari <mt1027319@gmail.com>
2023-04-29 23:44:49 +0530

    Starting backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDataBase = require("./config/database");
+
+
+dotenv.config({path: "backend/config/config.env"})
+
+
+connectDataBase();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+})
