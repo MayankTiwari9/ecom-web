064a82e55712e28578e66e2b9c0940c853b811b8
Mayank tiwari <mt1027319@gmail.com>
2023-05-12 00:32:14 +0530

    Admin All Products

--- frontend/src/Components/Admin/Products/AllProducts.js
+++ frontend/src/Components/Admin/Products/AllProducts.js
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./AllProducts.css";
 import Sidebar from '../Sidebar/Sidebar';
+import { Link } from 'react-router-dom';
+import {useDispatch, useSelector} from "react-redux";
+import {getAllProducts} from "../../../Redux/actions/productAction";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
 
 const AllProducts = () => {
+
+  const dispatch = useDispatch();
+
+  const {products} = useSelector((state) => state.allProducts); 
+
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  },[dispatch])
+
+  
+const rows: GridRowsProp = [];
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'Product Id', width: 210 }, 
+  { field: 'name', headerName: 'Name', width: 250 },
+  { field: 'price', headerName: 'Price', width: 150 },
+  { field: 'category', headerName: 'Category', width: 150 },
+  { field: 'stock', headerName: 'Stock', width: 150 },
+  { field: 'createdAt', headerName: 'Created At', width: 150 },
+];
+
+products && products.forEach((item) => {
+  rows.push({
+    id: item._id,
+    name: item.name,
+    price: item.price,
+    category: item.category,
+    stock: item.stock,
+    createdAt: item.createdAt
+  })
+})
+
   return (
-    <div className='dashboard'>
+    <div className='dashboard'> 
       <Sidebar/>
       <div className='admin-product-main'>
-        <div className='admin-product-heading'>
+        <div className='admin-product-heading'>  
           <h2>Product Inventory</h2>
-          <button>Create Product</button>
+          <button><Link to="/admin/product">+ Create Product</Link></button>
         </div>
+        {products && <div className='admin-product-over'>
+          <div>
+            <p>Total Products</p>
+            <h1>{products.length}</h1>
+          </div>
+          <div>
+            <p>Out Of Stock</p>
+            <h1>3</h1> 
+          </div>
+          <div>
+            <p>Categories</p>
+            <h1>4</h1>
+          </div>
+          <div>
+            <p>Sub Categories</p>
+            <h1>5</h1>
+          </div>
+        </div>}
+        
+        <div style={{ height: "27vmax", width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} initialState={{
+    pagination: { paginationModel: { pageSize: 5 } },
+  }}
+    pageSizeOptions={[5, 10, 25]}/>
+    </div>
       </div>
     </div>
   )
