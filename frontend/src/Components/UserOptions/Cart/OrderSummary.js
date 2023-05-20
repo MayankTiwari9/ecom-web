8cb574d84e67d4dd7251d9971270e94190b8a967
Mayank tiwari <mt1027319@gmail.com>
2023-05-21 00:00:22 +0530

    Order Summary Creating

--- frontend/src/Components/UserOptions/Cart/OrderSummary.js
+++ frontend/src/Components/UserOptions/Cart/OrderSummary.js
@@ -1,14 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {saveShippingInfo} from "../../../Redux/actions/cartAction";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {Country, State} from "country-state-city";
-
+import "./OrderSummary.css";
 
 
 const OrderSummary = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {shippingInfo} = useSelector((state) => state.cart);
+  
+  
+  const [address, setAddress] = useState(shippingInfo.address);
+  const [city, setCity] = useState(shippingInfo.city);
+  const [state, setState] = useState(shippingInfo.state);
+  const [country, setCountry] = useState(shippingInfo.country);
+  const [pinCode, setPinCode] = useState(shippingInfo.pinCode);
+  const [phoneNo, setPhoneNo] = useState(shippingInfo.phoneNo);
+
+  const shippingSubmit = (e) => {
+    e.preventDefault();
+    
+    if(phoneNo.length < 10 || phoneNo.length > 10){
+        alert.error("Phone Number Should be 10 Digit Long");
+        return;
+    }
+    dispatch(
+        saveShippingInfo({address, city,state,country, pinCode, phoneNo})
+    )
+    navigate("/order/confirm");
+
+}
+  
   return (
-    <div>OrderSummary</div>
+    <div>
+      <h2 className='shippingHeadingMain'>Order Summary</h2>
+      <div className='shippingContainer'>
+                <div className='shippingBox'>
+                  <div>
+
+                    <h2 className='shippingHeading'>Shipping Details</h2>
+                    <form className='shippingForm' encType='multipart/form-data' onSubmit={shippingSubmit} 
+                    >
+                        <div>
+                            <input type="text" placeholder='Address' required value={address} onChange={(e) => setAddress(e.target.value)} />
+                        </div>
+                        <div>
+                            <input
+                                type='text' placeholder='City' required value={city} onChange={(e) => setCity(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <input type='number' placeholder='Pin Code' required value={pinCode} onChange={(e) => setPinCode(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <input type="number" placeholder='Phone Number' required value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <select required value={country} onChange={(e) => setCountry(e.target.value)}>
+                                <option value="">Country</option>
+                                {Country && Country.getAllCountries().map((item) => (
+                                    <option key={item.isoCode} value={item.isoCode}>{item.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        {country && (
+                          <div>
+                                <select required value={state} onChange={(e) => setState(e.target.value)}>
+                                    <option value="">State</option>
+                                    {State &&
+                                        State.getStatesOfCountry(country).map((item) => (
+                                          <option key={item.isoCode} value={item.isoCode}>
+                                                {item.name}
+                                            </option>
+                                        ))}
+                                </select>
+                            </div>
+                        )}
+                        <input type="submit" value="Continue" className='shippingBtn' disabled={state ? false : true} />
+                    </form>
+                        </div>
+                        <div>
+                          Cart Items
+                        </div>
+                </div>
+                <div>
+                  Payment Summary
+                </div>
+            </div>
+            </div>
   )
 }
 
